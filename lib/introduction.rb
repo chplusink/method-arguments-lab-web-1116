05aa33dbde3851858290482b93d288b28fac5adf
Vic Lam <lam.vic.k@gmail.com>
2016-11-22 20:34:02 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,7 @@
+def introduction(nombre)
+  puts "Hi, my name is #{nombre}.\n"
+end
+
+def introduction_with_language(nombre, lengua)
+  puts "Hi, my name is #{nombre} and I am learning to program in #{lengua}.\n"
+end
